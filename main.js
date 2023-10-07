c35afbbf440d9dfd9910ce8708c8bcf4b6e621cf
IgorBoitsev <igor.boitsev@gmail.com>
2023-10-07 03:46:58 +0300

    init router

--- main.js
+++ main.js
@@ -1,23 +1,75 @@
 // import 'normalize.css';
 // import './style.scss';
+// import { Navigation, Thumbs } from 'swiper/modules';
+// import Swiper from 'swiper';
 
-import { Navigation, Thumbs } from 'swiper/modules';
-import Swiper from 'swiper';
+// const { default: Navigo } = require('navigo');
 
-const sliderThumbnails = new Swiper(".product__slider-thumbnails", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-var sliderMain = new Swiper(".product__slider-main", {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".product__arrow-next",
-    prevEl: ".product__arrow-prev",
-  },
-  modules: [Navigation, Thumbs],
-  thumbs: {
-    swiper: sliderThumbnails,
-  },
-});
+import Navigo from 'navigo';
+
+const productSlider = () => {
+  Promise.all([
+    import('swiper/modules'),
+    import('swiper'),
+    import('swiper/css')
+  ]).then(([{ Navigation, Thumbs }, Swiper]) => {
+
+    const sliderThumbnails = new Swiper.default(".product__slider-thumbnails", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+  
+    new Swiper.default(".product__slider-main", {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".product__arrow-next",
+        prevEl: ".product__arrow-prev",
+      },
+      modules: [Navigation, Thumbs],
+      thumbs: {
+        swiper: sliderThumbnails,
+      },
+    });
+
+  });
+};
+
+
+const init = () => {
+  productSlider();
+
+  const router = new Navigo('/', {linksSelector: 'a[href^="/"]'});
+
+  router
+    .on('/', () => {
+
+    })
+    .on('/category', () => {
+      
+    })
+    .on('/favorite', () => {
+      
+    })
+    .on('/search', () => {
+      
+    })
+    .on('/product/:id', () => {
+      
+    })
+    .on('/cart', () => {
+      
+    })
+    .on('/order', () => {
+      
+    })
+    .notFound(() => {
+      console.log(404);
+    });
+
+
+  router.resolve();
+};
+
+init();
